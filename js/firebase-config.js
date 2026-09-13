e06ea18643ac403004b28fd039c4982c4b2c7ec0
barba-productions <lhbarbarini@gmail.com>
2026-09-13 19:45:56 -0400

    Wire up real Firebase project config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,10 +2,10 @@
 // Firebase console -> Project settings -> General -> Your apps -> SDK setup and configuration.
 // See ../firebase/README.md for how to create the project.
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyBpDbHFXUyeHlftwisKDXf_sBbTUwYtJXA",
+  authDomain: "pokemon-desktop-online.firebaseapp.com",
+  projectId: "pokemon-desktop-online",
+  storageBucket: "pokemon-desktop-online.firebasestorage.app",
+  messagingSenderId: "784293488831",
+  appId: "1:784293488831:web:3fcb935aafbe6c7f0c7e0a",
 };
